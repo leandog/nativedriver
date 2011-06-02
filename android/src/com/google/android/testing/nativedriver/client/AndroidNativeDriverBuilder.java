8428b8f2d5ce972c0526ea3fdb5f904eea17244f
nativedriver.mirrorbot@gmail.com <nativedriver.mirrorbot@gmail.com@9ea60b42-7040-e045-bdf9-16ade6be0113>
2011-06-02 01:08:50 +0000

    Automated g4 rollback of changelist 21569208.
    *** Reason for rollback ***
    
    Flag is not included in the google common package in Selenium which fails the build of open source SkyTree (probably due to mis-config?)
    
    *** Original change description ***
    
    Port and host options were added for forwarding. Getting those info from system property.
    
    R=tkaizu,matvore,hnoda
    DELTA=43  (4 added, 36 deleted, 3 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2129
    
    
    git-svn-id: http://nativedriver.googlecode.com/svn/trunk@31 9ea60b42-7040-e045-bdf9-16ade6be0113

--- android/src/com/google/android/testing/nativedriver/client/AndroidNativeDriverBuilder.java
+++ android/src/com/google/android/testing/nativedriver/client/AndroidNativeDriverBuilder.java
@@ -19,9 +19,6 @@ package com.google.android.testing.nativedriver.client;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
-import com.google.common.flags.Flag;
-import com.google.common.flags.FlagSpec;
-import com.google.common.flags.InvalidFlagValueException;
 
 import org.openqa.selenium.remote.CommandExecutor;
 import org.openqa.selenium.remote.HttpCommandExecutor;
@@ -36,34 +33,23 @@ import javax.annotation.Nullable;
  *
  * @see AndroidNativeDriver#AndroidNativeDriver(CommandExecutor, AdbConnection)
  * @author Matt DeVore
- * @author Dezheng Xu
  */
 public class AndroidNativeDriverBuilder {
   private static final int DEFAULT_SERVER_PORT = 54129;
 
+  /**
+   * The URL used to connect to the server when using a constructor that does
+   * not take a {@code remoteAddress} or {@code executor} argument.
+   */
   private static final String DEFAULT_SERVER_URL
       = "http://localhost:" + DEFAULT_SERVER_PORT + "/hub";
 
-  @FlagSpec(help="Set the Android NativeDriver server URL")
-  private static final Flag<URL> FLAG_and_server_url;
-
-  static {
-    URL defaultServerUrl;
+  private static URL defaultServerUrl() {
     try {
-      defaultServerUrl = new URL(DEFAULT_SERVER_URL);
+      return new URL(DEFAULT_SERVER_URL);
     } catch (MalformedURLException exception) {
       throw Throwables.propagate(exception);
     }
-    FLAG_and_server_url = new Flag<URL>(defaultServerUrl) {
-      @Override
-      protected URL parse(String url) throws InvalidFlagValueException {
-        try {
-          return new URL(url);
-        } catch (MalformedURLException exception) {
-          throw new InvalidFlagValueException(url);
-        }
-      }
-    };
   }
 
   @Nullable private CommandExecutor commandExecutor;
@@ -75,18 +61,8 @@ public class AndroidNativeDriverBuilder {
     return this;
   }
 
-  /**
-   * Set the server using the command-line flag {@code and_server_url} if
-   * available. It can be specified by command-line:
-   *   <p><test_cases_binary> --and_server_url=<URL> <test_case_class_name>
-   *
-   * <p>If the command-line flag is not specified, the URL is set to
-   * {@code http://localhost:54129/hub}
-   *
-   * @see com.google.common.flags.Flags#parse(String[])
-   */
   public AndroidNativeDriverBuilder withDefaultServer() {
-    return withServer(FLAG_and_server_url.get());
+    return withServer(defaultServerUrl());
   }
 
   public AndroidNativeDriverBuilder withServer(URL url) {
